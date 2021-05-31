ed44fd0122dc82235e41c52b7d065ea5cb7a3b56
UnRealReincarlution <benwhite2413@gmail.com>
2021-05-31 18:59:46 +1200

    Working Static Camera Version

--- public/components/scene.tsx
+++ public/components/scene.tsx
@@ -1,29 +1,39 @@
-import { Canvas, MeshProps, useFrame } from '@react-three/fiber'
+import { Canvas, MeshProps, useFrame, extend, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
-import { useRef, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { Vector } from './vector'
+import ReactDOM from 'react-dom'
+
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { EffectComposer, Bloom, DepthOfField, Noise, Vignette } from '@react-three/postprocessing'
+// extend({ OrbitControls });
 
 const LORENZ_SIGMA = 10;
 const LORENZ_RHO = 28;
 const LORENZ_BETA = 8/3;
-const LORENZ_DELTA = 0.01;
+const LORENZ_DELTA = 0.005;
 
-const Sphere: React.FC<MeshProps> = ({ position }) => {
-	console.log(position);
-
-	const [ state, setState ] = useState(new Vector(position[0], position[1], position[2]));
+const Sphere: React.FC<MeshProps> = ({ info, callback }) => {
+	const [ state, setState ] = useState(info);
 	const mesh = useRef<THREE.Mesh>(null!);
 
 	useFrame(() => {
-		console.log(state);
+		const oldLocation = state.loc;
+		const newLocation = state.loc.rk4(LORENZ_DELTA, nextPoint);
 
-		setState(state.rk4(LORENZ_DELTA, nextPoint));
-	})
+		setState({ loc: newLocation, path: [ ...state.path, newLocation ]});
+	});
+
+	useEffect(() => {
+		callback(state);
+	}, [state])
+
+	console.log(state);
 
 	return (
 		<mesh
-			position={[state.x, state.y, state.z]}
-			ref={mesh}
+			position={[state.loc.x, state.loc.y, state.loc.z]}
+			ref={mesh}	
 		>
 
 			<sphereGeometry args={[.05, 3]} />
@@ -33,29 +43,70 @@ const Sphere: React.FC<MeshProps> = ({ position }) => {
 }
 
 const Scene = () => {
-	const points = [ 
-		new Vector(rand(10), rand(10), rand(10))
-	];
+	const [ travler, setTraveler ] = useState({
+		loc: new Vector(rand(.1), rand(.1), rand(.1)),
+		path: []
+	});
+
+	const points = useMemo(() => [new THREE.Vector3(-10, 0, 0), new THREE.Vector3(0, 10, 0), new THREE.Vector3(10, 0, 0)], [])
+	const onUpdate = useCallback(self => self.setFromPoints(points), [points])
+
+	const onTravelerUpdate = useCallback(self => {
+		self.setFromPoints(travler.path)
+	}, [travler])
 
     return (
-        <Canvas>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
+        <Canvas camera={{ fov: 75, position: [0, 0, 70]}} >
+			<Sphere info={travler} callback={setTraveler} key={Math.random() * 100} /> 	
 
-			{
-				points.map(e => (
-					<Sphere position={[e.x, e.y, e.z]} key={Math.random() * 100}/> 
-				))
-			}
+			<>
+				<line position={[0, -2.5, -10]}>
+					<bufferGeometry attach="geometry" onUpdate={onTravelerUpdate} />
+					<lineBasicMaterial attach="material" color={'#9c88ff'} linewidth={10} linecap={'round'} linejoin={'round'} />
+				</line>
+			</>
+
+			<EffectComposer>
+				<Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={20} />
+				<Noise opacity={0.02} />
+				<Vignette eskil={false} offset={0.1} darkness={1.1} />
+			</EffectComposer>
+
+			{/* <Dolly /> */}
         </Canvas>
     )
 }
 
+function Dolly() {
+	// This one makes the camera pitch up and down across the y axis focusing on the center
+	useFrame(({ clock, camera }) => {
+		camera.lookAt(0, 0, 0)
+		camera.position.y = 50 + Math.sin(clock.getElapsedTime()) * 30
+	})
+	return null
+}
+
+// const CameraControls = () => {
+// 	const {
+// 		camera,
+// 		gl: { domElement },
+// 	} = useThree();
+
+// 	// Ref to the controls, so that we can update them on every frame using useFrame
+// 	const controls = useRef();
+	
+// 	useFrame((state) => controls.current.update());
+	
+// 	return <orbitControls ref={controls} args={[camera, domElement]} />;
+// };
+
 var lorenzSystem = function (pos, sigma, rho, beta) {
+	// Calculate the delta x, y and z using the lorenz equations
 	var x = sigma * (pos.y - pos.x),
 		y = pos.x * (rho - pos.z) - pos.y,
 		z = pos.x * pos.y - (beta * pos.z);
 
+	// Return a new vector co-ordinate
 	return new Vector(x, y, z);
 };
 
